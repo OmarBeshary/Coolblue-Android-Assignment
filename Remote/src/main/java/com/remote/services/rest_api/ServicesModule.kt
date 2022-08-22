25f990b97bb5bf00605ac94c388955baadc293e0
OBeshary <omat.beshary@halan.com>
2022-08-23 01:09:04 +0200

    Inject SearchProductsService.kt

--- Remote/src/main/java/com/remote/services/rest_api/ServicesModule.kt
+++ Remote/src/main/java/com/remote/services/rest_api/ServicesModule.kt
@@ -1,8 +1,19 @@
 package com.remote.services.rest_api
 
 import com.remote.retrofit.RetrofitModule
+import com.remote.services.rest_api.search.SearchProductsService
 import dagger.Module
+import dagger.Provides
+import retrofit2.Retrofit
+import javax.inject.Singleton
 
 
 @Module(includes = [RetrofitModule::class])
-class ServicesModule
+class ServicesModule {
+
+    @Provides
+    @Singleton
+    fun provideSearchProductsService(retrofit: Retrofit): SearchProductsService =
+        retrofit.create(SearchProductsService::class.java)
+
+}
