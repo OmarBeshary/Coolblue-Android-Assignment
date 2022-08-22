a9ce322d1d506472bb24854ed3253b275eaca0ae
OBeshary <omat.beshary@halan.com>
2022-08-23 01:23:58 +0200

    Inject SearchProductsFragment,kt

--- app/src/main/java/com/coolblue_android_assignment/di/module/FragmentsModule.kt
+++ app/src/main/java/com/coolblue_android_assignment/di/module/FragmentsModule.kt
@@ -1,6 +1,15 @@
 package com.coolblue_android_assignment.di.module
 
+import com.coolblue_android_assignment.di.FragmentScope
+import com.coolblue_android_assignment.ui.search.SearchProductsFragment
 import dagger.Module
+import dagger.android.ContributesAndroidInjector
 
 @Module
-abstract class FragmentsModule
+abstract class FragmentsModule {
+
+    @FragmentScope
+    @ContributesAndroidInjector
+    abstract fun contributeLoginFragment(): SearchProductsFragment
+
+}
