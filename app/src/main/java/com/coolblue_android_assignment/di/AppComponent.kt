ea0ced43510caabe5f4193f780e21d042b4d75f6
OBeshary <omat.beshary@halan.com>
2022-08-23 01:25:02 +0200

    Update AppComponent.kt
    inject modules.

--- app/src/main/java/com/coolblue_android_assignment/di/AppComponent.kt
+++ app/src/main/java/com/coolblue_android_assignment/di/AppComponent.kt
@@ -5,13 +5,15 @@ import com.context.ContextModule
 import com.coolblue_android_assignment.App
 import com.coolblue_android_assignment.di.module.FragmentsModule
 import com.coolblue_android_assignment.di.module.SchedulersModule
+import com.coolblue_android_assignment.di.module.ViewModelsModule
+import com.data.di.module.RepositoryModule
 import dagger.BindsInstance
 import dagger.Component
 import dagger.android.AndroidInjectionModule
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [AndroidInjectionModule::class, ContextModule::class, SchedulersModule::class, FragmentsModule::class])
+@Component(modules = [AndroidInjectionModule::class, ContextModule::class, RepositoryModule::class, FragmentsModule::class, SchedulersModule::class, ViewModelsModule::class])
 interface AppComponent {
 
     fun inject(application: App)
